7bc015e57d661a64e8a8bb8e90fdbbfe1c3c2de9
Kachi <onlinehassle1234@gmail.com>
2024-11-22 07:53:37 -0600

    Fixed errors

--- src/lib/i18n/i18n.ts
+++ src/lib/i18n/i18n.ts
@@ -1,7 +1,7 @@
 import i18next from "i18next";
-import i18n from "i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
-import { initReactI18next } from "react-i18next";
+// import i18n from "i18next";
+// import LanguageDetector from "i18next-browser-languagedetector";
+// import { initReactI18next } from "react-i18next";
 
 i18next.init({
   lng: "de",
@@ -13,23 +13,3 @@ i18next.init({
     },
   },
 });
-// i18n
-//   .use(LanguageDetector)
-//   .use(initReactI18next)
-//   .init({
-//     debug: true,
-//     lng: "fr",
-//     // fallbackLng: ["en-US"],
-//     resources: {
-//       en: {
-//         translantion: {
-//           greeting: "Hello, Welcome!",
-//         },
-//       },
-//       fr: {
-//         translantion: {
-//           greeting: "Bonjour, Bienvenue !",
-//         },
-//       },
-//     },
-//   });
